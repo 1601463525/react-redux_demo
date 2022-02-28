41806d383c6de1c2eccdfa245335ad42c9c554c7
haojianfeng <1005881431@qq.com>
2022-02-28 10:42:12 +0800

    fix: REDUX_DEVTOOLS_EXTENSION

--- src/store/index.js
+++ src/store/index.js
@@ -7,4 +7,4 @@ import {createStore,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import reducers from './reducers'
 
-export default createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export default createStore(reducers,applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
